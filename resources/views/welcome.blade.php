a709bfa1a01ff6b71cff9e35ce0b9cc2bf966660
Miguel <miguel.dalessio@naranjax.com>
2020-10-25 22:51:02 -0300

    contacto cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>TecnoGoo</title>		
           <link rel="stylesheet" href="css/style.css">
           <link rel="stylesheet" href="css/parallax/parallax.css">
-          <link rel="stylesheet" href="css/contacto/contacto.css">
+          <link rel="stylesheet" href="css/contact/contact.css">
           <link rel="stylesheet" href="css/footer/footer.css">
           <link rel="stylesheet" href="css/whytecnogoo/whytecnogoo.css">
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
