f80153d9d783457ab0fb98b83dadb05c379afc2e
Miguel <miguel.dalessio@naranjax.com>
2020-10-26 21:26:55 -0300

    todos los cambios footer y whutecnogoo

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -1,36 +1,48 @@
-<footer class="text-center footer-style footer-content">
-        <div class="row">           
-            <div class="col-md-3 footer-col footer-contacto">
-                <h3 class="footer-contactoh3">Contacto</h3>
-                <p>
-                    info@tecnogoo.com<br />
-                    (AR) +54 911 23177217<br />
-                    (ES) +34 643 253200
-                </p>
-            </div>
-
-            <div class="vl"></div>
-
-            <div class="col-md-4 footer-col">
-                <h3>Redes Sociales</h3>
-                <ul class="list-inline">
-                    <li>
-						<a href="#" class="fa fa-facebook"></a>
-						<a href="#" class="fa fa-twitter"></a>
-						<a href="#" class="fa fa-instagram"></a>
-						<a href="#" class="fa fa-linkedin"></a>
-                    </li>
-                </ul>          
-            </div>
-
-            <div class="vl"></div>
-
-            <div class="col-md-3 footer-col">
-                <h3>Tecnogoo</h3>
-                <p>Desarrollo de software a medida, nos enfocamos en pequeñas empresas, ofreciendole soluciones a un costo muy bajo.</p>
-            </div>
-        </div>
-        <div class="derechos">
-        	<h1 class="footer-derechos">Copyright © 2019 - 2020 Tecnogoo. Todos los derechos reservados.</h1>
-    	</div>
-</footer>
+	<section id="footer">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Clientes</h5>
+					<ul class="list-unstyled quick-links">
+                        <li><a><i class="fa fa-angle-double-right"></i>Inicio</a></li>
+                        <li><a><i class="fa fa-angle-double-right"></i>Clientes</a></li>
+						<li><a><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Enlaces</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a><i class="fa fa-angle-double-right"></i>Quienes somos</a></li>
+						<li><a><i class="fa fa-angle-double-right"></i>Servicios</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Información de contacto</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a><i class="fa fa-angle-double-right"></i>(11) +56872828</a></li>
+						<li><a><i class="fa fa-angle-double-right"></i>(56) +911232332)</a></li>
+                        <li><a><i class="fa fa-angle-double-right"></i>info@tecnogoo.com</a></li>
+                        <li><a><i class="fa fa-angle-double-right"></i>hola@tecnogoo.com</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+				</div>
+				<hr>
+			</div>	
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p><u><a>Tecnogoo</a></u> registrada bajo el número 08938373, bajo declaración de tecnogoo soluciones, todos los derechos reservados.</p>
+					<p class="h6">© All right Reversed 2019-2020.<a class="text-green ml-2" href="https://www.tecnogoo.com" target="_blank">Tecnogoo</a></p>
+				</div>
+				<hr>
+			</div>	
+		</div>
+	</section>
--- resources/views/whytecnogoo/whytecnogoo.blade.php
+++ resources/views/whytecnogoo/whytecnogoo.blade.php
@@ -1,54 +1,92 @@
-<section id="why-tecnogoo">
-    <h1 class="title-principal-checks">¿Por qué TecnoGoo?</h1>
+<style>
+    .card-columns{
+    padding-top: 2%;
+    padding-right: 10%;
+    padding-left: 10%;
+}
+.title-whytecnogoo{
+    text-align: center;
+    padding-top: 2%;
+}
+.card-interna{
+    background-color: white;
+    border: 0;
 
-<div class="section-checks">
-    <div class="icon-checks">
-        <img class="img-checks" src="img/checklistWhy/emprendedores.png">
-    </div>
-    <div class="texto-why">
-        <h1 class ="title-checks">Nos enfocamos en emprendedores</h1>
-        <p class="parradfo-text-checks">Estamos enfocados en empresas en pleno crecimiento que requieran implementar o mejorar su stock tecnologico, permitiendo asi incrementar sus ventas y poder captar un mayor público</p> 
-    </div>
-</div>
+}
+.card-text{
+    color: black;
+}
+.text-parrafo{
+    color: black;
+}
+.iconos-whytecnogoo{
+    height: 5%;
+    width: 5%;
+    font-size: 92px;
+    color:darkblue;
+    padding-right: 28%;
+}
+.text-parrafo{
+    color:dimgray;
+}
+    </style>
 
-<div class="section-checks">
-    <div class="icon-checks">
-        <img class="img-checks" src="img/checklistWhy/rapidez.png">
-    </div>
-    <div class="texto-why">
-        <h1 class ="title-checks">Rapidez, Sencillez</h1>
-        <p class="parradfo-text-checks">Nos caracterizamos por responder en tiempo y forma, llevando a cabo la mejor solución para tu negocio y ofreciendo las mejores alternativas para mejorar la calidad de los productos.</p> 
-    </div>
+<div class="title-whytecnogoo">
+    <h2>¿Por qué Tecnogoo?</h2>
 </div>
-
-<div class="section-checks">
-    <div class="icon-checks">
-        <img class="img-checks" src="img/checklistWhy/soporte.png">
-    </div>
-    <div class="texto-why">
-        <h1 class ="title-checks">Soporte 24/7</h1>
-        <p class="parradfo-text-checks">Equipo de soporte dedicado para aclarar cualquier duda en cualquier momento, las 24 horas del día, los 7 días de la semana. Simplemente nos enfocamos en el cliente, para asi garantizar una mejor calidad en nuestros servicios.</p> 
-    </div>
-</div>
-
-<div class="section-checks">
-    <div class="icon-checks">
-        <img class="img-checks" src="img/checklistWhy/talento.png">
-    </div>
-    <div class="texto-why">
-        <h1 class ="title-checks">Talento</h1>
-        <p class="parradfo-text-checks">Contamos con equipos dedicados de desarrollo, diseño, QA. Para asi garantizar el mejor desarrollo con el mejor talento, dispuesto a buscar la mejor solución tecnológica.</p> 
-    </div>
-</div>
-
-<div class="section-checks">
-    <div class="icon-checks">
-        <img class="img-checks" src="img/checklistWhy/rapidez.png">
-    </div>
-    <div class="texto-why">
-        <h1 class ="title-checks">Concepto de TecnoGoo</h1>
-        <p class="parradfo-text-checks">El cliente como prioridad, nos caracterizamos por tener la prioridad enfocada en la necesidad del cliente en base a su negocio, responsabilidad y seguridad.</p> 
-    </div>
-</div>
-<br>
-</section>
+<div class="card-columns">
+    <div class="card card-interna">
+        <div class="card-body text-center">
+            <span class="material-icons iconos-whytecnogoo">
+                thumb_up_alt
+                </span>
+          <h5 class="card-text">Emprendedores</h5>
+          <p class="text-parrafo">Estamos enfocados en empresas en pleno crecimiento que requieran implementar o mejorar su stock tecnologico, permitiendo asi incrementar sus ventas y poder captar un mayor público</p>
+        </div>
+      </div>
+      <div class="card card-interna">
+        <div class="card-body text-center">
+            <span class="material-icons iconos-whytecnogoo">
+                local_airport
+                </span>
+          <h5 class="card-text">Rapidez y sencillez.</h5>
+          <p class="text-parrafo">Nos caracterizamos por responder en tiempo y forma, llevando a cabo la mejor solución para tu negocio y ofreciendo las mejores alternativas para mejorar la calidad de los productos.</p>
+        </div>
+      </div>
+      <div class="card card-interna">
+        <div class="card-body text-center">
+            <span class="material-icons iconos-whytecnogoo">
+                local_phone
+                </span>
+          <h5 class="card-text">Soporte 24/7</h5>
+          <p class="text-parrafo">Equipo de soporte dedicado para aclarar cualquier duda en cualquier momento, las 24 horas del día, los 7 días de la semana. Simplemente nos enfocamos en el cliente como prioridad.</p>
+        </div>
+      </div>
+      <div class="card card-interna">
+        <div class="card-body text-center">
+            <span class="material-icons iconos-whytecnogoo">
+                wc
+                </span>
+          <h5 class="card-text">Talento</h5>
+          <p class="text-parrafo">Contamos con equipos dedicados de desarrollo, diseño, QA. Para asi garantizar el mejor desarrollo con el mejor talento, dispuesto a buscar la mejor solución tecnológica.</p>
+        </div>
+      </div>
+      <div class="card card-interna">
+        <div class="card-body text-center">
+            <span class="material-icons iconos-whytecnogoo">
+                wc
+                </span>
+          <h5 class="card-text">Concepto de TecnoGoo</h5>
+          <p class="text-parrafo">El cliente como prioridad, nos caracterizamos por tener la prioridad enfocada en la necesidad del cliente en base a su negocio, responsabilidad y seguridad.</p>
+        </div>
+      </div>
+      <div class="card card-interna">
+        <div class="card-body text-center">
+            <span class="material-icons iconos-whytecnogoo">
+                cached
+                </span>
+          <h5 class="card-text">Precio / Calidad</h5>
+          <p class="text-parrafo">Garantizamos el mejor servicio al menor precio posible, obtimizando y priorizando los tiempos para entregar un producto de calidad con un costo bajo y en tiempo record.</p>
+        </div>
+      </div>
+  </div>
