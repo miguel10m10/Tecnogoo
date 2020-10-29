56d43055a9cd0a5939760b068f2822a6a3279583
JeffersonAvila <jeffavila91@gmail.com>
2020-10-29 20:19:51 +0100

    añadidos servicios y clientes

---
+++ resources/views/client/client.blade.php
@@ -0,0 +1,58 @@
+<div id="qsomos">
+    <h1 class="text-center mb-2 mt-2 ">Clientes</h1>
+</div>
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner" id="idCarousel">
+    <div class="carousel-item active" data-interval="3000">
+    <div class="card-service center   prueba" style="width: 17rem; margin-left: 15%;">
+        <img src="img/clientsImg/Banco-Pichincha1.png" class="card-img-top-quehacemos"
+            alt="...">
+        <div class="card-body">   
+        </div>
+    </div>
+    <div class="card-service center   prueba" style="width: 17rem;">
+        <img src="img/clientsImg/EstarSeguros.png" class="card-img-top-quehacemos"
+            alt="...">
+        <div class="card-body">
+        </div>
+    </div>
+    <div class="card-service center   prueba" style="width: 17rem;">
+        <img src="img/clientsImg/iumo1.png" class="card-img-top-quehacemos"
+            alt="...">
+        <div class="card-body">
+        </div>
+    </div>     
+     </div>
+    <div class="carousel-item" data-interval="3000">
+    <div class="card-service center  " style="width: 17rem; margin-left: 15%;">
+        <img src="img/clientsImg/logo-banesco.png" class="card-img-top-quehacemos"
+            alt="...">
+        <div class="card-body">
+  
+        </div>
+    </div>
+    <div class="card-service center  " style="width: 17rem;">
+        <img src="img/clientsImg/logo-pluspetrol.png" class="card-img-top-quehacemos"
+            alt="...">
+        <div class="card-body">
+        </div>
+    </div>
+    <div class="card-service center  " style="width: 17rem;">
+        <img src="img/clientsImg/logodigitel.png" class="card-img-top-quehacemos"
+            alt="...">
+        <div class="card-body">
+        </div>
+    </div> 
+        </div>
+  </div>
+  
+
+</div>
+
+<!-- ------------------------------------------------------------------------------------ -->
+
+
