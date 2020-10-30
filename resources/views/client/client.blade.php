2db0644af65aefbeaf89b61ec7b34c4d3078e2d0
Miguel <miguel.dalessio@naranjax.com>
2020-10-29 21:23:34 -0300

    ajuste carrusel clientes

--- resources/views/client/client.blade.php
+++ resources/views/client/client.blade.php
@@ -6,13 +6,13 @@
     <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
         <div class="carousel-inner row w-10 mxauto" role="listbox">
             <div class="carousel-item carousel-item-first col-md-4 active">
-                <img class="img-fluid mx-auto d-bloc" src="img/clientsImg/iumo1.png" alt="slide 1">
+                <img class="img-fluid mx-auto d-bloc" src="img/clientsImg/Claragua1.png" alt="slide 1">
             </div>
             <div class="carousel-item col-md-4">
                 <img class="img-fluid mx-auto d-block" src="img/clientsImg/EstarSeguros.png" alt="slide 2">
             </div>
             <div class="carousel-item col-md-4">
-                <img class="img-fluid mx-auto d-block" src="img/clientsImg/logodigitel.png" alt="slide 3">
+                <img class="img-fluid mx-auto d-block" src="img/clientsImg/donaemi.png" alt="slide 3">
             </div>
             <div class="carousel-item col-md-4">
                 <img class="img-fluid mx-auto d-block" src="img/clientsImg/logo-banesco.png" alt="slide 4">
@@ -21,13 +21,16 @@
                 <img class="img-fluid mx-auto d-block" src="img/clientsImg/logo-pluspetrol.png" alt="slide 5">
             </div>
             <div class="carousel-item col-md-4">
-                <img class="img-fluid mx-auto d-block" src="img/clientsImg/logodigitel.png" alt="slide 6">
+                <img class="img-fluid mx-auto d-block" src="img/clientsImg/iumo1.png" alt="slide 6">
             </div>
             <div class="carousel-item col-md-4">
-                <img class="img-fluid mx-auto d-block" src="img/clientsImg/iumo1.png" alt="slide 7">
+                <img class="img-fluid mx-auto d-block" src="img/clientsImg/LOGOVALEVEN.png" alt="slide 7">
             </div>
             <div class="carousel-item col-md-4">
-                <img class="img-fluid mx-auto d-block" src="img/clientsImg/iumo1.png" alt="slide 8">
+                <img class="img-fluid mx-auto d-block" src="img/clientsImg/oleica1.png" alt="slide 8">
+            </div>
+            <div class="carousel-item col-md-4">
+                <img class="img-fluid mx-auto d-block" src="img/clientsImg/roros1.png" alt="slide 9">
             </div>
         </div>
         <a class="carousel-control-prev" role="button" data-slide="prev" data-target="#carouselExample">
