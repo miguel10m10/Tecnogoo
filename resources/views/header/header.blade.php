213d7b84fc2f734f0941b1027ac7ecafb7456591
Miguel <miguel.dalessio@naranjax.com>
2020-11-08 19:04:08 -0300

    ajuster logo

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,18 +1,18 @@
 <header>
   <nav id="nav">
+   <div class="logo">
+      <img src="img/TG_5.png" alt="logo"/>
+   </div>
      <div class="menu-icon">
         <i class="fa fa-bars fa-2x"></i>
-     </div>
-     <div class="logo">
-        Tecnogoo
      </div>
      <div class="menu">
         <ul>
            <li><a href="#">Inicio</a></li>
-           <li><a href="#">Tecnogoo</a></li>
-           <li><a href="#">Clientes</a></li>
-           <li><a href="#">Tecnologías</a></li>
-           <li><a href="#">Contacto</a></li>
+           <li><a href="#whytecnogoo">Tecnogoo</a></li>
+           <li><a href="#clientes">Clientes</a></li>
+           <li><a href="#whytecnogoo">Tecnologías</a></li>
+           <li><a href="#contacto">Contacto</a></li>
         </ul>
      </div>
   </nav>
